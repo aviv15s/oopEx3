4ed9dcfc5a124b2c58d18051b97d6f095c907e4c
aviv15s <aviv.shemesh.43@gmail.com>
2024-02-28 11:04:20 +0200

    upload

---
+++ ascii_art/AsciiArtAlgorithm.java
@@ -0,0 +1,30 @@
+package ascii_art;
+
+import ascii_output.AsciiOutput;
+
+/**
+ * מחלקה זו תהיה אחראית על הרצת
+ * האלגוריתם. הפרמטרים עבור ריצת האלגוריתם יתקבלו בבנאי של המחלקה. הריצה תהיה תלויה כמובן
+ * בתמונה שנבחרה, ברזולוציה שנבחרה )כמה תווים יהיו בשורה בתוצאה(, וכן בסט התווים שנבחר. הדרך
+ * שבה נתונים אלו יתורגמו לפרמטרים של המחלקה תלוי במימוש שלכם לחלקים הקודמים
+ */
+public class AsciiArtAlgorithm {
+
+    /**
+     * constructor for class
+     * @param resolution number of sum-images in each row
+     */
+    public AsciiArtAlgorithm(int resolution){
+        // resolution is the number of sub-images (chars) in each row. the number of
+        // chars in each column is given by the resolution and the image size (each char is a square)
+    }
+
+    /**
+     * runs the algorithm and returns the char array
+     * @return char array representing image
+     */
+    public char [][] run(){
+
+    }
+
+}
--- image_char_matching/SubImgCharMatcher.java
+++ image_char_matching/SubImgCharMatcher.java
@@ -1,17 +1,39 @@
 package image_char_matching;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * שתהיה אחראית להתאים תו ASCII לתת תמונה עם בהירות נתונה. מחלקה זו תשמש את אלגוריתם
  * אומנות ה-ASCII שלנו בהמשך כדי להחליף תתי תמונות בתווים.
  */
 public class SubImgCharMatcher {
-
+    private final boolean WHITE_PIXEL = true;
+    private HashMap<Character, Float> charSet;
     /**
      * Constructor
      * @param charset charset used by the algorithm
      */
     public SubImgCharMatcher(char[] charset){
+        this.charSet = new HashMap<Character, Float>();
+        for (int i = 0; i < charset.length; i++) {
+            char c = charset[i];
+            float charBrightness = calculateCharBrightness(c);
+            this.charSet.put(c, charBrightness);
+        }
+    }
+
+    private float calculateCharBrightness(char c){
+        boolean[][] pixelArray = CharConverter.convertToBoolArray(c);
+        int numberOfWhitePixels = 0;
+        for (int i = 0; i < pixelArray.length; i++) {
+            for (int j = 0; j < pixelArray[i].length; j++) {
+                if (pixelArray[i][j] == WHITE_PIXEL) {
+                    numberOfWhitePixels++;
+                }
+            }
+        }
 
     }
 
@@ -29,7 +51,7 @@ public class SubImgCharMatcher {
      * @param c char
      */
     public void addChar(char c){
-
+        this.charSet.add(c);
     }
 
     /**
@@ -37,7 +59,7 @@ public class SubImgCharMatcher {
      * @param c char
      */
     public void removeChar(char c){
-
+        this.charSet.remove(c);
     }
 
 
