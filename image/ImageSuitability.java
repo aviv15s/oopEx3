8f8c212330b8aef709cbdd5d0663d44192fbf296
ramGoldstein <ram.goldstein.43@gmail.com>
2024-02-28 19:37:11 +0200

    finish

--- image/ImageSuitability.java
+++ image/ImageSuitability.java
@@ -10,7 +10,7 @@ import static java.lang.Math.max;
  * class that helps us control on the grey object comfortably.
  */
 public class ImageSuitability {
-    private static final int  MAX_RGB = 255;
+    private static final int MAX_RGB = 255;
     private final double[][] greyImage;
     private double[][] greyLevelsSubImages;
     private int sizeSquare;
@@ -35,6 +35,7 @@ public class ImageSuitability {
         setGreyLevelsSubImages();
 
     }
+
     /**
      * Sets a new resolution for the ImageSuitability instance.
      * If the new resolution differs from the current one, it recalculates the sizeSquare,
@@ -42,8 +43,8 @@ public class ImageSuitability {
      *
      * @param newResolution The new resolution to be set.
      */
-    public void setResolution(int newResolution){
-        if (newResolution != resolution){
+    public void setResolution(int newResolution) {
+        if (newResolution != resolution) {
             resolution = newResolution;
             sizeSquare = greyImage.length / resolution;
             numImagesCol = greyImage[0].length / sizeSquare;
@@ -51,62 +52,97 @@ public class ImageSuitability {
         greyLevelsSubImages = new double[resolution][numImagesCol];
         setGreyLevelsSubImages();
     }
+
     /**
      * Populates the greyLevelsSubImages array by calculating the grey levels for each subimage.
      * This method iterates through the subimage grid and uses the greyLevelCalculatorSubImage method
      * to determine the grey level for each subimage and stores the result in the corresponding array element.
      */
-    private void setGreyLevelsSubImages(){
+    private void setGreyLevelsSubImages() {
         for (int indexRow = 0; indexRow < resolution; indexRow++) {
             for (int indexCol = 0; indexCol < numImagesCol; indexCol++) {
-                greyLevelsSubImages[indexRow][indexCol] = greyLevelCalculatorSubImage(indexRow,indexCol);
+                greyLevelsSubImages[indexRow][indexCol] = greyLevelCalculatorSubImage(indexRow, indexCol);
             }
         }
     }
 
     /**
      * calculate for specific subImage the greyness
+     *
      * @param indexRow row to start from
      * @param indexCol col to look from
      * @returngreness of that subImage
      */
-    private double greyLevelCalculatorSubImage(int indexRow, int indexCol){
+    private double greyLevelCalculatorSubImage(int indexRow, int indexCol) {
         double sum = 0;
-        for (int row = 0; row < sizeSquare; row++){
-            for (int col = 0; col < sizeSquare; col++){
-                sum += greyImage[row+indexRow*sizeSquare][col+indexCol*sizeSquare];
+        for (int row = 0; row < sizeSquare; row++) {
+            for (int col = 0; col < sizeSquare; col++) {
+                sum += greyImage[row + indexRow * sizeSquare][col + indexCol * sizeSquare];
             }
         }
-        return sum/(MAX_RGB* sizeSquare*sizeSquare);
+        return sum / (MAX_RGB * sizeSquare * sizeSquare);
     }
 
+    /**
+     * get resolution
+     *
+     * @return resolution
+     */
     public int getresolution() {
         return resolution;
     }
 
+    /**
+     * get greyLevelsSubImages
+     *
+     * @return greyLevelsSubImages
+     */
     public double[][] getGreyLevelsSubImages() {
         return greyLevelsSubImages;
     }
 
+    /**
+     * get numImagesCol
+     *
+     * @return numImagesCol
+     */
     public int getnumImagesCol() {
         return numImagesCol;
     }
 
-    static class PaddingGreyImage{
+    public static class PaddingGreyImage {
         private static final Color DEFAULT_COLOR = Color.WHITE;
-        public static double[][] setgreyImage(Image image){
+        private static final double RED_FACTOR = 0.2126;
+        private static final double BLUE_FACTOR = 0.0722;
+        private static final double GREEN_FACTOR = 0.7152;
+
+        /**
+         * Converts a given color image to a grayscale representation.
+         *
+         * @param image The input color image.
+         * @return A 2D array representing the grayscale image.
+         */
+        public static double[][] setgreyImage(Image image) {
             double[][] greyImage = new double[image.getWidth()][image.getHeight()];
-            for (int row = 0; row < image.getWidth(); row++){
-                for (int col = 0 ; col < image.getHeight(); col++){
-                    Color currentPixel = image.getPixel(row,col);
-                    greyImage[row][col] = currentPixel.getRed()*0.2126 +
-                            currentPixel.getGreen()*0.7152 +
-                            currentPixel.getBlue()*0.0722;
+            for (int row = 0; row < image.getWidth(); row++) {
+                for (int col = 0; col < image.getHeight(); col++) {
+                    Color currentPixel = image.getPixel(row, col);
+                    greyImage[row][col] = currentPixel.getRed() * RED_FACTOR +
+                            currentPixel.getGreen() * GREEN_FACTOR +
+                            currentPixel.getBlue() * BLUE_FACTOR;
                 }
             }
             return greyImage;
         }
-        public static Image createImageRightSizes(Image originalImage){
+
+        /**
+         * Creates a new image with dimensions adjusted to the nearest power of 2.
+         * Pads the image with the default color if necessary.
+         *
+         * @param originalImage The input image to be adjusted.
+         * @return A new image with adjusted dimensions.
+         */
+        public static Image createImageRightSizes(Image originalImage) {
             int[] sizes = rightSizes(originalImage.getWidth(), originalImage.getHeight());
             int width = sizes[0];
             int height = sizes[1];
@@ -118,19 +154,33 @@ public class ImageSuitability {
             return new Image(pixelArray, width, height);
         }
 
-
+        /**
+         * Fills a given pixel array with the default color, then copies the pixels from the
+         * original image to the center of the array.
+         *
+         * @param pixelArray    The pixel array to be filled and modified.
+         * @param originalImage The original image to copy pixels from.
+         * @param diffCol       The difference in columns between arrays.
+         * @param diffRow       The difference in rows between arrays.
+         */
         private static void fillingPixelArray(Color[][] pixelArray, Image originalImage, int diffCol, int diffRow) {
             for (Color[] e : pixelArray) {
                 Arrays.fill(e, DEFAULT_COLOR);
             }
             for (int row = diffRow / 2; row < pixelArray.length - diffRow / 2; row++) {
                 for (int col = diffCol / 2; col < pixelArray[row].length - diffCol / 2; col++) {
-                    pixelArray[row][col] = originalImage.getPixel(row-diffRow/2, col-diffCol/2);
+                    pixelArray[row][col] = originalImage.getPixel(row - diffRow / 2, col - diffCol / 2);
                 }
             }
         }
 
-
+        /**
+         * Calculates the nearest power of 2 for width and height and returns as an array.
+         *
+         * @param width  The original width.
+         * @param height The original height.
+         * @return An array containing the adjusted width and height.
+         */
         private static int[] rightSizes(int width, int height) {
             int[] rightSizes = new int[2];
             int biggerNumber = 1;
