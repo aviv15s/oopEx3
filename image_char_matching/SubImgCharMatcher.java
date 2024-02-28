b231185ea8e4fa3420bca6a9b342d47c53474536
aviv15s <aviv.shemesh.43@gmail.com>
2024-02-28 10:30:05 +0200

    upload

---
+++ image_char_matching/SubImgCharMatcher.java
@@ -0,0 +1,46 @@
+package image_char_matching;
+
+
+/**
+ * שתהיה אחראית להתאים תו ASCII לתת תמונה עם בהירות נתונה. מחלקה זו תשמש את אלגוריתם
+ * אומנות ה-ASCII שלנו בהמשך כדי להחליף תתי תמונות בתווים.
+ */
+public class SubImgCharMatcher {
+
+    /**
+     * Constructor
+     * @param charset charset used by the algorithm
+     */
+    public SubImgCharMatcher(char[] charset){
+
+    }
+
+    /**
+     * used to get the char with the closest brightness to the given one
+     * @param brightness brightness to match
+     * @return a char
+     */
+    public char getCharByImageBrightness(double brightness){
+
+    }
+
+    /**
+     * adds a char to the charset
+     * @param c char
+     */
+    public void addChar(char c){
+
+    }
+
+    /**
+     * removes a char from the charset
+     * @param c char
+     */
+    public void removeChar(char c){
+
+    }
+
+
+
+
+}
